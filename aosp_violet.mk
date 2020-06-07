21e93c5635ad241e4e80db8aa0acf28fcdf0dc80
bluejeans117 <vishnurajesh45@gmail.com>
2020-06-07 15:21:44 +0530

    violet: move makefile calls to device makefile

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -4,12 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
