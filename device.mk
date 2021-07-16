7dd65f7203f51d305d07a73d904bee262c45c208
Lunarixus <Nathan@lunarixus.dev>
2021-07-16 15:41:02 +0530

    [SQUASH]violet: Remove packages at build time
    Squash of the following:
    
    violet: remove_packages: Rename RemovePackages for source
    
    violet: remove_packages: Switch to platform certificate
    
    Co-authored-by: naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
    Co-authored-by: Arturo <rtsanonynousperson@gmail.com>
    Signed-off-by: princejb786 <sam.jbjin@gmail.com>

--- device.mk
+++ device.mk
@@ -367,6 +367,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
