cbd88fded83879f1823d988261fd926b02215013
Mir Yasin Ali <miryasin@m.ms.evolution-x.org>
2022-06-30 12:14:41 +0530

    violet: vendorsetup.sh: Refactor
    Signed-off-by: Mir Yasin Ali <miryasin@m.ms.evolution-x.org>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,18 @@
+# Stuffs to rm -rf
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/display
+
+# Hals
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/audio
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_media.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/media
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_display.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/display
+
+# Vendor Tree
 git clone --depth=1 https://github.com/EvolutionXViolet/vendor_xiaomi_violet.git vendor/xiaomi/violet
+
+# Kernel Tree
 git clone --depth=1 https://github.com/EvolutionXViolet/Azure-caf-4.14.git kernel/xiaomi/violet
+
+# Firmware
 git clone --depth=1 https://gitlab.com/princejb786/vendor_xiaomi-firmware_violet.git vendor/xiaomi-firmware/violet
