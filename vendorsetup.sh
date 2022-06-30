068f82b94c7cd83536a689fa01285c86929f6ac6
Mir Yasin Ali <miryasin@m.ms.evolution-x.org>
2022-06-30 12:01:04 +0530

    violet: vendorsetup.sh: Add vendor and kernel tree * Yes because i am too lazy  ;)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,5 @@ rm -rf hardware/qcom-caf/sm8150/display
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/audio
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_media.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/media
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_display.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone --depth=1 https://github.com/EvolutionXViolet/vendor_xiaomi_violet.git vendor/xiaomi/violet
+git clone --depth=1 https://github.com/EvolutionXViolet/Azure-caf-4.14.git kernel/xiaomi/violet
