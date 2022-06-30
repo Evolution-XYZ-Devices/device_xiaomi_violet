c6600b9ef451cb14def456692765b81cd7594a8c
Mir Yasin Ali <miryasin@m.ms.evolution-x.org>
2022-06-30 11:44:34 +0530

    violet: Drop GoogleCamera Go

--- device.mk
+++ device.mk
@@ -143,7 +143,6 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
-    GoogleCameraGo \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
