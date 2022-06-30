15c296e5249257f1c6d6d782125386cd176136f1
Mir Yasin Ali <miryasin@m.ms.evolution-x.org>
2022-06-30 11:54:56 +0530

    violet: Add vendorsetup.sh * Cuz i always forget to replace hals everytime i sync
    Signed-off-by: Mir Yasin Ali <miryasin@m.ms.evolution-x.org>

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/media
+rm -rf hardware/qcom-caf/sm8150/display
+git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/audio
+git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_media.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/media
+git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_display.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/display
